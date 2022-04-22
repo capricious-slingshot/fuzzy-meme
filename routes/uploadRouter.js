8213a54f37023927ab598aabc8316c878e077676
capricious-slingshot <ephemere@protonmail.com>
2022-04-21 18:24:50 -0700

    added multer and router for uploading images

---
+++ routes/uploadRouter.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const authenticate = require('../authenticate')
+const multer = require('multer')
+const uploadRouter = express.Router()
+
+
+//CUSTOM CONFIGURATION
+
+//STORAGE
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    //error status, path to save file to
+    cb(null, 'public/images')
+  },
+  filename: (req, file, cb) => {
+    //error status, sets name of client file to name of file on server
+    //if this isn't configured manualy multer will default to random stirng for file name
+    cb(null, file.originalname)
+  }
+})
+
+//IMAGE ONLY FILE TYPE
+const imageFileFilter = (req, file, cb) => {
+  //checks file extention
+  if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('You can upload only image files!'), false)
+  }
+  cb(null, true)
+}
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter })
+
+uploadRouter.route('/')
+  //POST is the only allowed path
+  //shouldn't the the other requests be simplified into one? splat?
+
+  .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403
+    res.end('GET operation not supported on /imageUpload')
+  })
+
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.json(req.file)
+  })
+
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403
+    res.end('PUT operation not supported on /imageUpload')
+  })
+
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403
+    res.end('DELETE operation not supported on /imageUpload')
+  })
+
+module.exports = uploadRouter
